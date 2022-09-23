b42150b693e1cbb11691d794f8f786c8d95d70c5
mrvlyouknowwho <108728264+mrvlyouknowwho@users.noreply.github.com>
2022-09-23 20:43:17 +0500

    Update initialTaskM9.cpp

--- SkillboxDraft/initialTaskM9.cpp
+++ SkillboxDraft/initialTaskM9.cpp
@@ -4,6 +4,7 @@
 std::string alphabetU = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //0-25
 std::string alphabetL = "abcdefghijklmnopqrstuvwxyz";
 
+
 int counter(std::string l) // How many letters in a word, filters out other characters
 {
    int x = 0; // counter
@@ -19,21 +20,54 @@ int counter(std::string l) // How many letters in a word, filters out other char
 // Returns the correct number needed to set the counter as an index 
 //----------------------------------------------------------------------//
 
+bool reg(char c) // check registor
+{
+   bool capital;
+
+   for (int i = 0; i != 26; i++) // 25 - length of alphabet
+   {
+      if (c == alphabetU[i]) capital = true;
+   }
+   for (int i = 0; i != 26; i++) // 25 - length of alphabet
+   {
+      if (c == alphabetL[i]) capital = false;
+   }
+   return capital;
+}
+// Case input check, works in tandem with shift function
+//----------------------------------------------------------------------//
+
 char shift(char c) // shift by one
 {
    int counter = 1;
-   for (int i = 0; c != alphabetU[i]; i++)
+   if (reg(c))
    {
-      counter++;
-      if (counter == 26) counter = 0; // 25 - length of alphabet
+      for (int i = 0; c != alphabetU[i]; i++)
+      {
+         counter++;
+         if (counter == 26) counter = 0; // 25 - length of alphabet
+      }
+      c = alphabetL[counter];
    }
-   return alphabetL[counter];
+   else
+   {
+      for (int i = 0; c != alphabetL[i]; i++)
+      {
+         counter++;
+         if (counter == 26) counter = 0; // 25 - length of alphabet
+      }
+      c = alphabetL[counter];
+   }
+   return c;
 }
-// Shift by one
+// Shift by one depends on the execution of the reg function
 //----------------------------------------------------------------------//
+
 
 std::string assembly(std::string l)
 {
+   //char temp;
+   
    for (int i = 0; i != counter(l); i++)  l[i] = shift(l[i]);
    return l;
 }
