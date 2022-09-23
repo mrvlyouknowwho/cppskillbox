66a775ec033a0c2aa5a99f27f4976dea60b02d03
mrvlyouknowwho <108728264+mrvlyouknowwho@users.noreply.github.com>
2022-09-23 23:19:27 +0500

    Update initialTaskM9.cpp

--- SkillboxDraft/initialTaskM9.cpp
+++ SkillboxDraft/initialTaskM9.cpp
@@ -12,35 +12,10 @@ std::string encrypt(std::string l)
    return l;
 }
 
-std::string decrypt(std::string l, bool reg)
-{
-   for (int x = 0; x != l.length(); x++)
-   {
-      if (reg) // If True - must be decrypted in lowercase letters
-      {
-         if (l[x] > 'a' && l[x] <= 'z')    l[x] = l[x]--;
-         else if (l[x] == 'a' || l[x] == 'A')   l[x] = 'z';
-      }
-      else // If False - must be decrypted in capital letters
-      {
-         if (l[x] > 'a' && l[x] <= 'z')    l[x] = l[x]-33; 
-         else if (l[x] == 'a' || l[x] == 'A')   l[x] = 'Z';
-      }
-   }
-   return l;
-}
-
 int main()
 {
-   bool reg;
    std::string line;
-   std::cout << "Enter you word, tap enter and choice parametrs for decrypter\n";
-   std::cout << "0 = capital letter | 1 = lowercase letter\n";
-   std::cin >> line >> reg;
+   std::cin >> line;
 
-   std::cout << "\n-----------------------------------------------------------------------\n";
-   std::cout << "Your encrypted string: " << encrypt(line) << "\n";
-   std::cout << "-----------------------------------------------------------------------\n";
-   std::cout << "Your decrypted string: " << decrypt(encrypt(line), reg);
-   std::cout << "\n-----------------------------------------------------------------------\n\n";
+   std::cout << encrypt(line);
 }
