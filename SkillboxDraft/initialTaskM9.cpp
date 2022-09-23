4a0902f59794275c87e36c4b68567c770a6a3724
mrvlyouknowwho <108728264+mrvlyouknowwho@users.noreply.github.com>
2022-09-23 20:22:30 +0500

    Update initialTaskM9.cpp

--- SkillboxDraft/initialTaskM9.cpp
+++ SkillboxDraft/initialTaskM9.cpp
@@ -20,63 +20,23 @@ int counter(std::string l) // How many letters in a word, filters out other char
 // Returns the correct number needed to set the counter as an index 
 //----------------------------------------------------------------------//
 
-char split(std::string l, int x) //Splitting into characters
-{
-   return l[x];
-}
-// Splits a specific number into a separate character, for further work
-//----------------------------------------------------------------------//
-
-bool reg(char c) // check registor
-{
-   bool capital;
-
-   for (int i = 0; i != 26; i++) // 25 - length of alphabet
-   {
-      if (c == alphabetU[i]) capital = true;
-      else if (c == alphabetL[i]) capital = false;
-   }
-   return capital;
-}
-// Case input check, works in tandem with shift function
-//----------------------------------------------------------------------//
 
 char shift(char c) // shift by one
 {
    int counter = 1;
-   if (reg(c))
+   for (int i = 0; c != alphabetU[i]; i++)
    {
-      for (int i = 0; c != alphabetU[i]; i++)
-      {
-         counter++;
-         if (counter == 26) counter = 0; // 25 - length of alphabet
-      }
-      c = alphabetL[counter];
+      counter++;
+      if (counter == 26) counter = 0; // 25 - length of alphabet
    }
-   else
-   {
-      for (int i = 0; c != alphabetL[i]; i++)
-      {
-         counter++;
-         if (counter == 26) counter = 0; // 25 - length of alphabet
-      }
-      c = alphabetL[counter];
-   }
-   return c;
+   return alphabetL[counter];
 }
-// Shift by one depends on the execution of the reg function
+// Shift by one
 //----------------------------------------------------------------------//
 
 std::string assembly(std::string l)
 {
-   char temp;
-   
-   for (int i = 0; i != counter(l); i++)
-   {
-      temp = split(l, i);
-      temp = shift(temp);
-      l[i] = temp;
-   }
+   for (int i = 0; i != counter(l); i++)  l[i] = shift(l[i]);
    return l;
 }
 // Finished string assembly
