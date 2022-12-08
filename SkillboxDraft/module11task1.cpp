f7ef6860d7eb5c05c799ed1ee4a607b4dec1df46
mrvlyouknowwho <108728264+mrvlyouknowwho@users.noreply.github.com>
2022-12-08 20:46:12 +0600

    fix done

--- SkillboxDraft/module11task1.cpp
+++ SkillboxDraft/module11task1.cpp
@@ -4,7 +4,12 @@
 std::string encrypt(std::string l, int step)
 {
     for (int x = 0; x != l.length(); x++)
-        if (l[x] >= 65 && l[x] <= 122)                       l[x] = l[x] + step;
+    {
+        if (l[x] >= 'A' && l[x] <= 'Z')
+            step >= 0 ? l[x] = 'A' + ((l[x] - 'A' + step) % 26) : l[x] = 'Z' + ((l[x] - 'Z' + step) % 26);
+        if (l[x] >= 'a' && l[x] <= 'z')
+            step >= 0 ? (l[x] = 'a' + (l[x] - 'a' + step) % 26) : l[x] = 'z' + ((l[x] - 'z' + step) % 26);
+    }
     return l;
 }
 
@@ -15,5 +20,6 @@ int main()
     std::getline(std::cin, l);
     std::cin >> step;
 
-    std::cout << encrypt(l, step);
+    //std::cout << encrypt(l, step) << std::endl;
+    std::cout << encrypt(l, -step) << std::endl;
 }
