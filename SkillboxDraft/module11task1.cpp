c49ba8216d6cd6bcbbd56dad898a71042b061bd5
mrvlyouknowwho <108728264+mrvlyouknowwho@users.noreply.github.com>
2022-12-09 00:18:04 +0600

    Update module11task1.cpp

--- SkillboxDraft/module11task1.cpp
+++ SkillboxDraft/module11task1.cpp
@@ -15,11 +15,20 @@ std::string encrypt(std::string l, int step)
 
 int main()
 {
-    int step;
+    std::cout << "What do you want from me?\n";
+    std::cout << "0 - encrypt.\nanything - decoding\n";
+    char choice;
+    std::cin >> choice;
+    choice != '0' ? choice = false : choice = true;
+    std::cin.ignore(32767, '\n');
+
+    std::cout << "Please enter a line :\n";
     std::string l;
     std::getline(std::cin, l);
+
+    std::cout << "Please enter a step :\n";
+    int step;
     std::cin >> step;
 
-    //std::cout << encrypt(l, step) << std::endl;
-    std::cout << encrypt(l, -step) << std::endl;
+    choice == true ? std::cout << encrypt(l, step) : std ::cout << encrypt(l, -step);
 }
