29d7295884a68d18bea3da5c0d97a073c68d1d57
mrvlyouknowwho <108728264+mrvlyouknowwho@users.noreply.github.com>
2022-12-16 03:24:40 +0600

    fix

--- SkillboxDraft/Module11Task3.cpp
+++ SkillboxDraft/Module11Task3.cpp
@@ -1,103 +1,54 @@
 #include <iostream>
 #include <string>
 
-bool valid(const std::string &octet)
+void print_answer(const std::string &answer)
 {
-    for(int i = 0; i < octet.length(); i++)
-        if(!isdigit(octet[i])) return false;
+    std::cout << answer << std::endl;
+    exit(0);
+}
 
-    int check = 0;
-    try {
-        check = std::stoi(octet);
-    }
-    catch(std::invalid_argument){
-        return false;
-    }
-
-    std::string x = std::to_string(check);
-
-    if(x.length() != octet.length())
+void validate_octet(const std::string &octet)
+{
+    int check = std::stoi(octet);
+    if (std::to_string(check).length() != octet.length() || check > 255)
     {
-        return false;
-    }
-
-
-    if(check >= 0 && check <= 255)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
+        print_answer(std::string("This octet is not valid: ").append(octet));
     }
 }
 
-std::string get_address_part(const std::string &address, int part)
+void validate_ip(const std::string &address)
 {
-    std::string address_part1, address_part2, address_part3, address_part4;
-    int i = 0;
-    for(;i < address.length(); i++)
-    {
-        if(address[i] == '.') break;
-        address_part1 += address[i];
-    }
-    i++;
-    for(;i < address.length(); i++)
-    {
-        if(address[i] == '.') break;
-        address_part2 += address[i];
-    }
-    i++;
-    for(;i < address.length(); i++)
-    {
-        if(address[i] == '.') break;
-        address_part3 += address[i];
-    }
-    i++;
-    for(;i < address.length(); i++)
-    {
-        if(address[i] == '.') break;
-        address_part4 += address[i];
-    }
-    if(part == 1) return address_part1;
-    else if(part == 2) return address_part2;
-    else if(part == 3) return address_part3;
-    else if(part == 4) return address_part4;
-}
+    if(address.length() > 15 ||address.length() <= 0) print_answer(std::string("Address's size is invalid"));
 
-void answer(const std::string &address)
-{
-    int dote_count = 0;
-    for(int i = 0; i < address.length(); i++)
+    int dot_count = 0;
+    std::string string;
+
+    for(auto symbol: address)
     {
-        if(address[i] == '.') dote_count++;
+        if(symbol == '.')
+        {
+            string.length() != 0 ? validate_octet(string) : print_answer(("One of the octets is empty!"));
+            string.clear();
+            dot_count++;
+        }
+        else
+        {
+            if(isdigit(symbol))
+            {
+                string += symbol;
+            }
+            else print_answer(std::string("This symbol is not digit: ") += symbol);
+        }
     }
-    if(     valid(get_address_part(address, 1)) &&
-            valid(get_address_part(address, 2)) &&
-            valid(get_address_part(address, 3)) &&
-            valid(get_address_part(address, 4)) &&
-            dote_count == 3)
-    {
-        std::cout << "Valid!" << std::endl;
-    }
-    else
-    {
-        std::cout << "Invalid!" << std::endl;
-    }
+    if(string.length() != 0) validate_octet(string);
+    dot_count != 3 ? print_answer("Amount of octets is incorrect!") : print_answer(("IP address is valid!"));
 }
 
 int main()
 {
     std::string address;
+
     std::cout << "Enter IP:" << std::endl;
     std::cin >> address;
-
-    if(address.length() > 15)
-    {
-        std::cout << "Invalid!" << std::endl;
-    }
-    else
-    {
-        answer(address);
-    }
+    validate_ip(address);
 }
